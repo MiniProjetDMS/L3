a5a54e40cc3a95c4dd14f2b4c866df3b15f042ed
Nadir <Nadir@DESKTOP-DELL>
2020-05-01 16:36:38 +0100

    connection will done

--- src/mini_projet/Mini_Projet.java
+++ src/mini_projet/Mini_Projet.java
@@ -1,6 +1,3 @@
-/*
- * 
- */
 package mini_projet;
 
 import javafx.application.Application;
@@ -11,10 +8,12 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-/**
- *
- * 
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Mini_Projet extends Application {
     
     private double xOffset = 0;
@@ -24,9 +23,7 @@ public class Mini_Projet extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("LoginAs.fxml"));
         
-        Scene scene = new Scene(root);
-        
-        
+        Scene scene = new Scene(root);   
         
         stage.setScene(scene);
         stage.resizableProperty().set(false);
@@ -37,7 +34,6 @@ public class Mini_Projet extends Application {
 		xOffset = event.getSceneX();
 		yOffset = event.getSceneY();
 	});
-
 
 	//move around here
 	root.setOnMouseDragged((MouseEvent event) -> {
@@ -52,8 +48,33 @@ public class Mini_Projet extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        
+        Connection conn = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver"); 
+            // db parameters
+            String url       = "jdbc:mysql://localhost:3306/clinique_dentaire";
+            String user      = "root";
+            String password  = "";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            //****
+            if(conn != null)
+                System.out.println("cnx Done !!");
+            // for testing :
+            Statement stmt=conn.createStatement();  
+            ResultSet rs=stmt.executeQuery("select * from medcin");  
+            while(rs.next())  
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+            // ...
+	
+        } catch(SQLException e) {
+           System.out.println("dommage erreur ::"+e.getMessage());
+        } 
+        
         launch(args);
+        
     }
     
 }
